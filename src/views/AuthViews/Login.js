addeeaa32c4ca9ad97a4bc7fa8d7460683f47495
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2022-12-09 03:55:28 +0300

    updates

--- src/views/AuthViews/Login.js
+++ src/views/AuthViews/Login.js
@@ -155,15 +155,15 @@ const Container = styled(Box)`
         }
     }
     .background {
-        height: 100%;
+        height: 100vh;
         background-image: url(${backgroundImage});
         background-fit: cover;
         background-repeat: no-repeat;
         background-position: left top;
 
         img {
-            width: 100%;
-            height: 100%;
+            width: 100vw;
+            height: 100vh;
             object-fit: cover;
         }
     }
